95ebfa0655694cff1f1151589d90e21d0ecddabd
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-06-14 12:30:56 -0300

    Configurando gihub actions - 3.1: Create database

--- 3.1-create-database/rds-postgres/main.tf
+++ 3.1-create-database/rds-postgres/main.tf
@@ -1,9 +1,6 @@
 data "terraform_remote_state" "remote-state-vpc" {
   backend = "s3"
   config = {
-    shared_credentials_files = ["~/aws-dev-cli-admin-credentials.txt"]
-    profile                  = "default"
-
     region = "us-east-1"
     bucket = "remote-state-odoo"
     key    = "create-network/terraform.tfstate"
